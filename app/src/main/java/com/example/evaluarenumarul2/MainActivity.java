2aaf1ef0f9142734faa6af9d62681d7c5c261393
Dorin Boca <dorin.boca24@gmail.com>
2023-03-23 01:38:31 +0200

    Evaluare

---
+++ app/src/main/java/com/example/evaluarenumarul2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.evaluarenumarul2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                int length = editText.length();
+                String convert = String.valueOf(length);
+                textView.setText(convert);
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+    }
+}
